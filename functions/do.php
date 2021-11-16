c1ee964b0427866d77582595ce48ea860d0d532e
DanijelSVK <stopka.d@gmail.com>
2021-11-16 10:14:58 +0100

    feat: add embed html function

--- functions/do.php
+++ functions/do.php
@@ -8,3 +8,17 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Returns embed html.
+ *
+ * @param string $venue_slug
+ *
+ * @return string
+ */
+function bi_get_embed_html( $venue_slug = "" ) {
+	return "<div id=\"bookingit_embed\"></div>
+    <script src=\"https://bookingit.io/embed/embed.js\"></script>
+    <script type=\"text/javascript\">
+        bn({\"store\": \"$venue_slug\", \"logo\": false})
+    </script>";
+}
